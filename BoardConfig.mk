69addd345cc063d3e977ede874ce957d7ad1c188
Louis Popi <theh2o64@gmail.com>
2016-04-20 19:09:02 +0200

    gemini: If (Xiaomi_is_shit) { build_the_kernel_from_caf_sources; } else { fuck_u; }

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,6 +50,21 @@ TARGET_NO_BOOTLOADER := true
 TARGET_USES_C2D_COMPOSITION := true
 
 # Kernel
+ifneq ($(XIAOMI_IS_SHIT),true)
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+TARGET_CUSTOM_KERNEL_HEADERS := device/xiaomi/gemini/kernel-headers/
+TARGET_CUSTOM_KERNEL_HEADERS := kernel/xiaomi/msm8996/include/
+TARGET_KERNEL_APPEND_DTB := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CONFIG := msm_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8996
+TARGET_PREBUILT_KERNEL := device/xiaomi/gemini/kernel-dtb
+TARGET_USES_UNCOMPRESSED_KERNEL := true
+else
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=16M@0-0xffffffff androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image
@@ -58,6 +73,8 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_CUSTOM_KERNEL_HEADERS := kernel/xiaomi/msm8996/include/
 TARGET_KERNEL_APPEND_DTB := true
 TARGET_PREBUILT_KERNEL := device/xiaomi/gemini/kernel-dtb
+endif # XIAOMI_IS_SHIT
+
 
 
 # Partitions
